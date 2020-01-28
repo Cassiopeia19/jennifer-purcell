572229e35fca14552eb55dc583928a9ddd3fdfc5
Jennie <cassiopeia5571@gmail.com>
2020-01-28 17:18:34 -0600

    added portfolio code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Projects from "./pages/ProjectsPage";
+import Contact from "./pages/ContactPage";
+import SingleProject from "./pages/SingleProjectPage";
+import Languages from "./pages/LanguagesPage"
+
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+
+class App extends Component {
+
+  render() {
+    return (
+      <>
+
+        {/* navbar, sidebar,footer*/}
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/languages" component={Languages} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/projects" exact component={Projects} />
+          <Route path="/projects/:id" component={SingleProject} />
+        </Switch>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
+
---
+++ src/context/socialData.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+export const socialData = [
+         {
+           id: 1,
+           icon: <FaLinkedin className="icon" />,
+           url: "https://www.linkedin.com/in/jenniferpurcell2codeis2live"
+         },
+
+         {
+           id: 2,
+           icon: <FaGithub className="icon" />,
+           url: "https://github.com/Cassiopeia19"
+         }
+       ];
